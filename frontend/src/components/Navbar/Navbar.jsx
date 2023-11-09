25c44ffb6fb59412d79457242a308d9c3b8249e0
Nicola Gasparotto <84458448+NicolaGasparotto@users.noreply.github.com>
2023-11-09 16:58:54 +0100

    almost done the Navbar Component

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
 import './Navbar.css'; // Make sure to import the CSS file
 
+import politoLogo from '../../assets/politoLogo.png';
+
 const Navbar = () => {
   const [isMobile, setIsMobile] = useState(false);
 
   return (
     <nav className="navbar">
-      <div className="logo">Politecnico di Torino</div>
+      <div className="logo">
+        <img src={politoLogo} className='logo'/>
+      </div>
       <ul className={`nav-links ${isMobile && "mobile"}`}>
         
         <li><a href="/academics">Academics</a></li>
