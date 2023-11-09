131f3e7aa068d672c189bf6cadbb4faedcd03f6d
Nicola Gasparotto <84458448+NicolaGasparotto@users.noreply.github.com>
2023-11-09 15:30:55 +0100

    basic structure and some basic components initial style

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import PageNotFound from "./pages/PageNotFound";
 
 function App() {
   return (
---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import './Navbar.css'; // Make sure to import the CSS file
+
+const Navbar = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  return (
+    <nav className="navbar">
+      <div className="logo">Politecnico di Torino</div>
+      <ul className={`nav-links ${isMobile && "mobile"}`}>
+        
+        <li><a href="/academics">Academics</a></li>
+        <li><a href="/thesis">Thesis</a></li>
+        <li><a href="/innovation">Innovation</a></li>
+        
+        <li><button className="login-button">Login</button></li>
+      </ul>
+      <button className="mobile-nav-toggle" onClick={() => setIsMobile(!isMobile)}>
+        {isMobile ? 'x' : '≡'}
+      </button>
+    </nav>
+  );
+};
+
+export default Navbar;
