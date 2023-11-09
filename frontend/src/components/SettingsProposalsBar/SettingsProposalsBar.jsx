95a582cc6cdf21db8713a5103401aafa40c0b494
Nicola Gasparotto <84458448+NicolaGasparotto@users.noreply.github.com>
2023-11-09 19:42:09 +0100

    css for the filter button done

--- frontend/src/components/SettingsProposalsBar/SettingsProposalsBar.jsx
+++ frontend/src/components/SettingsProposalsBar/SettingsProposalsBar.jsx
@@ -1,22 +1,59 @@
-// React component for the webpage
-import React from "react";
-import "./SettingsProposalsBar.css"; // make sure to have the correct path to your CSS file
+import React, { useState } from "react";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import FilterAltIcon from "@mui/icons-material/FilterAlt";
 
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import FilterAltIcon from '@mui/icons-material/FilterAlt';
+import "./SettingsProposalsBar.css";
 
 const SettingsProposalsBar = () => {
+  const [showFilters, setShowFilters] = useState(false);
+
+  const toggleFilters = () => {
+    setShowFilters(!showFilters);
+  };
+
   return (
     <div className="content">
       <div className="search-bar">
         <input type="text" placeholder="Search proposal..." />
-        <button className="filters-button">
-          <FilterAltIcon />
-          Filters 
-          <KeyboardArrowDownIcon />
-        </button>
+        <div className="filter-container">
+          <button className="filters-button" onClick={toggleFilters}>
+            <FilterAltIcon />
+            Filters
+            <KeyboardArrowDownIcon />
+          </button>
+          {showFilters && <FiltersDropdown />}
+        </div>
         <button className="create-proposal-button">+ Create proposal</button>
       </div>
+    </div>
+  );
+};
+
+const FiltersDropdown = () => {
+  return (
+    <div className="filters-dropdown">
+      <div className="filter-section">
+        <h4>Filter 1:</h4>
+        <label>
+          <input type="checkbox" /> Option 1
+        </label>
+        <label>
+          <input type="checkbox" /> Option 2
+        </label>
+        <label>
+          <input type="checkbox" /> Option 3
+        </label>
+      </div>
+      <div className="filter-section">
+        <h4>Filter 2:</h4>
+        <label>
+          <input type="checkbox" /> Option 1
+        </label>
+        <label>
+          <input type="checkbox" /> Option 2
+        </label>
+      </div>
+      <button className="apply-filters">Apply</button>
     </div>
   );
 };
