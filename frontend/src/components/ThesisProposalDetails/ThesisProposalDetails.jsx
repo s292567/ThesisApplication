f5ba6c1a43643690890401d0d82a5a716b6e67b5
Nicola Gasparotto <84458448+NicolaGasparotto@users.noreply.github.com>
2023-11-13 11:45:28 +0100

    child modal component done it needs a lil restyling of buttons

--- frontend/src/components/ThesisProposalDetails/ThesisProposalDetails.jsx
+++ frontend/src/components/ThesisProposalDetails/ThesisProposalDetails.jsx
@@ -6,7 +6,7 @@ import Modal from "@mui/material/Modal";
 import Divider from "@mui/material/Divider";
 import Grid from "@mui/material/Grid";
 import IconButton from "@mui/material/IconButton";
-import CancelOutlinedIcon from '@mui/icons-material/CancelOutlined';
+import CancelOutlinedIcon from "@mui/icons-material/CancelOutlined";
 
 const modalStyle = {
   position: "absolute",
@@ -25,6 +25,17 @@ const modalStyle = {
   overflowY: "auto",
 };
 
+const modalButtonStyle = {
+  fontSize: 'large !important',
+    fontWeight: 'bold !important',
+    borderRadius: '12px !important',
+    border: 'none',
+    background: 'whitesmoke !important',
+    cursor: 'pointer ',
+    padding: '0.5rem 1rem !important', // Adjust padding as needed
+    marginRight: '1rem !important',
+};
+
 const formatDate = (date) => {
   const tmp = date.split("-");
   const day = tmp[2];
@@ -36,8 +47,16 @@ const formatDate = (date) => {
 /**                             { proposal } the proposal will be passed down to the element from the caller  */
 export default function ThesisProposalDetails() {
   const [open, setOpen] = React.useState(false);
+  const [childOpen, setChildOpen] = React.useState(false);
+
   const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+  const handleClose = () => {
+    setOpen(false);
+    setChildOpen(false); // Close child modal as well when parent is closed
+  };
+
+  const handleApply = () => setChildOpen(true);
+  const handleChildClose = () => setChildOpen(false);
 
   const proposal = {
     id: "P12345",
@@ -88,18 +107,18 @@ export default function ThesisProposalDetails() {
             onClick={handleClose}
             sx={{
               position: "absolute",
-              right: '1rem',
-              top: '1rem',
+              right: "1rem",
+              top: "1rem",
               color: "black", // You can change the color
             }}
           >
-            <CancelOutlinedIcon sx={{fontSize: "xx-large"}}/>
+            <CancelOutlinedIcon sx={{ fontSize: "xx-large" }} />
           </IconButton>
           <Typography
             id="modal-modal-title"
             variant="h6"
             component="h2"
-            sx={{ mt:2, textAlign: "center" }}
+            sx={{ mt: 2, textAlign: "center" }}
           >
             {proposal.title}
           </Typography>
@@ -200,6 +219,57 @@ export default function ThesisProposalDetails() {
               *N.B: some fields are not present in this proposal*
             </Typography>
           )}
+
+          <Box sx={{ display: "flex", justifyContent: "center", mt: '5rem' }}>
+            <Button sx={modalButtonStyle} onClick={handleClose} >
+              Close
+            </Button>
+            <Button sx={modalButtonStyle} onClick={handleApply} >Apply</Button>
+          </Box>
+        </Box>
+      </Modal>
+
+      {/* Nested Child Modal */}
+      <Modal open={childOpen} onClose={handleChildClose}>
+        <Box
+          sx={{
+            ...modalStyle,
+            maxHeight: "25vh",
+            width: "50%",
+            "@media (max-width: 768px)": {
+              padding: 4,
+              marginTop: 5,
+              marginBottom: 5,
+            },
+            "@media (min-width: 1024px)": {
+              width: "25%",
+            },
+          }}
+        >
+          <IconButton
+            aria-label="close"
+            onClick={handleChildClose}
+            sx={{
+              position: "absolute",
+              right: "1rem",
+              top: "1rem",
+              color: "black",
+            }}
+          >
+            <CancelOutlinedIcon sx={{ fontSize: "xx-large" }} />
+          </IconButton>
+          <Typography variant="h6" sx={{ mt: 2, textAlign: "center" }}>
+            ATTENTION
+          </Typography>
+          <Typography sx={{ textAlign: "center", mt: 2 }}>
+            Are you sure you want to apply to this thesis?
+          </Typography>
+
+          {/* Nested Modal Action Buttons */}
+          <Box sx={{ display: "flex", mt: 2, justifyContent: "center" }}>
+            <Button sx={modalButtonStyle} onClick={handleChildClose}>X</Button>
+            <Button  sx={modalButtonStyle} onClick={handleClose}>YES</Button>
+          </Box>
         </Box>
       </Modal>
     </div>
