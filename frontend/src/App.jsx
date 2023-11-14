f700f2fb3a0a60d687a34aad8202d170608eb96c
s292567 <101062653+s292567@users.noreply.github.com>
2023-11-14 20:21:06 +0100

    try to merge

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,27 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import PageNotFound from "./pages/PageNotFound";
+import StudentDashboardPage from "./pages/StudentDashBoardPage";
+import ProfessorDashboardPage from "./pages/ProfessorDashboardPage";
+import LoginPage from "./pages/LoginPage";
+
 function App() {
   return (
-      <>
-        <BrowserRouter>
-          <Routes>
+    <>
+      <BrowserRouter>
+        <Routes>
             <Route index element={<LandingPage />} />
             <Route path="/login" element={<LoginPage />} />
-
+            
             {/** TO GO THE SPECIFIC ROUTE YOU NEED TO PUT professorDashboard/1 */}
             <Route path="/ProfessorDashboard/:professorId" element={<ProfessorDashboardPage />} />
             {/*<Route path="/ProfessorDashboard-:professorId/CreateNewProposal" element={<... />} />*/ }
-
+            
             <Route path="/studentDashboard/:studendId" element={<StudentDashboardPage />} />
-
+            
             <Route path="*" element={<PageNotFound />} />
-          </Routes>
-        </BrowserRouter>
-      </>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
