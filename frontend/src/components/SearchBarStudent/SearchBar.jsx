1cb7f99e8fbeb3836438268bb33d425734d1a430
Nicola Gasparotto <84458448+NicolaGasparotto@users.noreply.github.com>
2023-11-15 09:55:47 +0100

    search bar main logic DONE search bar styling TO BE DONE

--- frontend/src/components/SearchBarStudent/SearchBar.jsx
+++ frontend/src/components/SearchBarStudent/SearchBar.jsx
@@ -1,6 +1,57 @@
-import { useState } from "react";
-import { Modal, Box, Button } from "@mui/material";
-import { styled } from "@mui/material/styles";
+import React, { useState } from "react";
+import {
+  AppBar,
+  Toolbar,
+  InputBase,
+  IconButton,
+  Button,
+  Modal,
+  Box,
+} from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import FilterListIcon from "@mui/icons-material/FilterList";
+import { styled, alpha } from "@mui/material/styles";
+
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(1),
+    width: "auto",
+  },
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: "12ch",
+      "&:focus": {
+        width: "20ch",
+      },
+    },
+  },
+}));
 
 const RoundShapeLabel = styled("div")(({ checked }) => ({
   borderRadius: "15px",
@@ -13,7 +64,9 @@ const RoundShapeLabel = styled("div")(({ checked }) => ({
   display: "inline-block",
 }));
 
-const CheckBoxes = () => {
+const SearchAppBar = () => {
+  const [searchInput, setSearchInput] = useState("");
+
   const defaultChecked = {
     title: false,
     supervisor: false,
@@ -29,10 +82,91 @@ const CheckBoxes = () => {
     Bsc: false,
     cds: false,
   };
-  
+
   const [checked, setChecked] = useState({ ...defaultChecked });
   const [open, setOpen] = useState(false);
 
+  const handleSearch = () => {
+    let searchParams = Object.keys(checked).reduce((acc, key) => {
+      if (checked[key] && key !== "Msc" && key !== "Bsc") {
+        acc[key] = searchInput;
+      }
+      return acc;
+    }, {});
+
+    // Handle Msc and Bsc fields separately
+    // i'm not sure HOW TO DO THAT.... it depepnds on the backend
+    if (checked.Msc || checked.Bsc) {
+      searchParams.level = [];
+      if (checked.Msc) {
+        searchParams.level.push("Msc");
+      }
+      if (checked.Bsc) {
+        searchParams.level.push("Bsc");
+      }
+    }
+
+    if ((Object.keys(searchParams).length === 0)) {
+      // Perform normal search query
+      getSearchCall(searchInput);
+    } else {
+      // Perform search query with filters
+      getSearchCall(searchParams);
+    }
+  };
+
+  const getSearchCall = (queryParams) => {
+    console.log("Search Query:", queryParams);
+    // Implement your search query logic here
+  };
+
+  return (
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ "aria-label": "search" }}
+              onChange={(e) => setSearchInput(e.target.value)}
+            />
+          </Search>
+          <IconButton onClick={handleSearch} color="inherit">
+            <SearchIcon /> search
+          </IconButton>
+          <Button
+            color="inherit"
+            onClick={() => {
+              setOpen(true);
+            }}
+          >
+            <FilterListIcon />
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <CheckBoxes
+        defaultChecked={defaultChecked}
+        checked={checked}
+        setChecked={setChecked}
+        open={open}
+        setOpen={setOpen}
+        handleSearch={handleSearch}
+      />{" "}
+    </div>
+  );
+};
+
+const CheckBoxes = ({
+  defaultChecked,
+  checked,
+  setChecked,
+  open,
+  setOpen,
+  handleSearch,
+}) => {
   const handleCheckChange = (key) => {
     setChecked({ ...checked, [key]: !checked[key] });
   };
@@ -41,7 +175,8 @@ const CheckBoxes = () => {
     setChecked({ ...defaultChecked });
   };
 
-  const handleClose = () => {
+  const handleApply = () => {
+    handleSearch();
     setOpen(false);
   };
 
@@ -58,8 +193,12 @@ const CheckBoxes = () => {
 
   return (
     <div>
-      <Button onClick={() => setOpen(true)}>Open Modal</Button>
-      <Modal open={open} onClose={handleClose}>
+      <Modal
+        open={open}
+        onClose={() => {
+          setOpen(false);
+        }}
+      >
         <Box sx={style}>
           <Box
             sx={{
@@ -79,11 +218,11 @@ const CheckBoxes = () => {
             ))}
           </Box>
           <Button onClick={handleClear}>Clear Filters</Button>
-          <Button onClick={handleClose}>Apply</Button>
+          <Button onClick={handleApply}>Apply</Button>
         </Box>
       </Modal>
     </div>
   );
 };
 
-export default CheckBoxes;
+export default SearchAppBar;
