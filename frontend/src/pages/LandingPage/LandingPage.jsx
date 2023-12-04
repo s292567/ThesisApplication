109c217961bed035f1c71908a3aea421da77dcbe
Nicola Gasparotto <84458448+NicolaGasparotto@users.noreply.github.com>
2023-12-04 20:29:25 +0100

    landing page responsive layout

--- frontend/src/pages/LandingPage/LandingPage.jsx
+++ frontend/src/pages/LandingPage/LandingPage.jsx
@@ -1,89 +1,105 @@
-import {Stack, Box, styled, Paper, Card, CardMedia, CardActionArea, Typography} from "@mui/material";
+import {
+  Stack,
+  Box,
+  styled,
+  Paper,
+  Card,
+  CardMedia,
+  CardActionArea,
+  Typography,
+} from "@mui/material";
 
-import politoView from '../../assets/images/politoView.jpg';
-import politoBiblio from '../../assets/images/politoBiblio.jpeg';
-
-const MyBox = styled(Box)({
-  display: 'flex',
-  position: 'relative', // Set the position to relative
-  alignItems: 'center', // Align items vertically
-  justifyContent: 'center', // Align items horizontally
-  flexGrow: '1'
-});
-
-const StyledPaper = styled(Paper)(({theme}) => ({
-  position: 'absolute', // Set the position to absolute
-  left: theme.spacing(6), // Offset from the left
-  top: theme.spacing(6), // Offset from the top
-  maxWidth: '400px', // Max width of the Paper
-  padding: theme.spacing(2), // Padding inside the Paper
-  backgroundColor: '#ffffffeb', // Background color
-  borderRadius: '18px', // Rounded corners
-  zIndex: 1 // Ensure it sits above the Card
-}));
+import politoView from "../../assets/images/politoView.jpg";
+import politoBiblio from "../../assets/images/politoBiblio.jpeg";
 
 export default function LandingPage() {
+  const cardData = [
+    {
+      image: politoBiblio,
+      title: "Discover the New Biblio",
+      description:
+        "Explore the vast resources and modern spaces of our newly opened library at Politecnico di Torino. A hub of knowledge and innovation.",
+    },
+    {
+      image: politoView,
+      title: "Experience the Campus Life",
+      description:
+        "Immerse yourself in the vibrant campus life at Politecnico di Torino. Engage with diverse cultures and cutting-edge technology.",
+    },
+  ];
+
   return (
-    <Box sx={{height: 'max-content', flexDirection: 'column', justifyContent: 'center'}}>
-      <Box sx={{display: 'flex', justifyContent: 'center', padding: '2rem', marginTop: '2rem'}}>
-        <Typography variant='h2' component='h1' sx={{color: '#003576', fontWeight: 'bold'}}>
-          Welcome @ Politecnico di Torino
-        </Typography>
-      </Box>
-      <Stack sx={{
-        display: 'flex',
-        padding: '2rem',
-        gap: '4rem',
-        height: 'max-content',
-        flexDirection: {xs: 'column', lg: 'row'},
-      }}>
-        <MyBox>
-          <Card sx={{display: 'flex', maxWidth: '800px', borderRadius: '18px'}} elevation={5}>
-            <CardActionArea>
-              <CardMedia
-                component='img'
-                height='600'
-                image={politoBiblio}
-                title="Biblio Polito"
-              />
-            </CardActionArea>
-          </Card>
-          <StyledPaper>
-            <Typography variant='h4' component='h2'>
-              Discover the New Biblio
-            </Typography>
-            <Typography>
-              Explore the vast resources and modern spaces of our newly opened library at Politecnico di Torino. A hub
-              of
-              knowledge and innovation.
-            </Typography>
-          </StyledPaper>
-        </MyBox>
+    <Box
+      sx={{
+        display: "flex",
+        height: "max-content",
+        flexDirection: "column",
+        justifyContent: "center",
+        flexWrap: "wrap",
+        alignContent: "center",
+      }}
+    >
+      <Typography
+        variant="h2"
+        component="h1"
+        sx={{
+          color: "#003576",
+          fontWeight: "bold",
+          padding: "1rem",
+          display: "flex",
+          flexDirection: "column",
+          flexWrap: "wrap",
+          alignContent: "center",
+          marginTop: "1rem",
+        }}
+      >
+        Welcome @ Politecnico di Torino
+      </Typography>
 
-
-        <MyBox>
-          <Card sx={{display: 'flex', maxWidth: '800px', borderRadius: '18px'}} elevation={5}>
-            <CardActionArea>
-              <CardMedia
-                component='img'
-                height='600'
-                image={politoView}
-                title="View Polito"
-              />
-            </CardActionArea>
-          </Card>
-          <StyledPaper>
-            <Typography variant='h4' component='h2'>
-              Life at Politecnico di Torino
-            </Typography>
-            <Typography>
-              Experience the vibrant campus life at one of Italy's most prestigious universities. An environment
-              fostering
-              innovation and community.
-            </Typography>
-          </StyledPaper>
-        </MyBox>
+      <Stack
+        sx={{
+          display: "flex",
+          padding: "2rem",
+          gap: "4rem",
+          height: "max-content",
+          flexDirection: { xs: "column", lg: "row" },
+        }}
+      >
+        {cardData.map((card, index) => (
+          <Box sx={{ position: "relative" }}>
+            <Card
+              sx={{ display: "flex", maxWidth: "800px", borderRadius: "18px" }}
+              elevation={5}
+            >
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  height="600"
+                  image={card.image}
+                  title={`Image ${index + 1}`}
+                />
+              </CardActionArea>
+            </Card>
+            <Paper
+              sx={{
+                position: "absolute",
+                top: "2rem",
+                left: "2rem",
+                zIndex: 10,
+                maxWidth: { sm: "400px", xs: "200px" },
+                padding: "1rem",
+                backgroundColor: "#ffffffeb",
+                borderRadius: "18px",
+              }}
+            >
+              <Typography variant="h4" component="h2">
+                {card.title}
+              </Typography>
+              <Typography>{card.description}</Typography>
+            </Paper>
+          </Box>
+        ))}
       </Stack>
     </Box>
-  )
+  );
 }
