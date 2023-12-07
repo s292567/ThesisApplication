d45294a3e080f37b020c46b43fbae7744f33ba4f
Romina Nemati <s309769@studenti.polito.it>
2023-12-07 20:07:57 +0100

    cypress basic works

--- frontend/src/cypress.config.js
+++ frontend/src/cypress.config.js
@@ -1,6 +1,26 @@
+/* eslint-disable no-undef */
+// eslint-disable-next-line no-undef
 module.exports = {
     projectId: "tj1vgw",
     // ...rest of the Cypress project config
 }
 
 // run the command "./node_modules/.bin/cypress open"
+const { defineConfig } = require('cypress')
+// Populate process.env with values from .env file
+
+
+require('dotenv').config()
+
+module.exports = defineConfig({
+    env: {
+
+        auth_username: process.env.AUTH_USERNAME,
+
+        auth_password: process.env.AUTH_PASSWORD,
+
+        okta_domain: process.env.REACT_APP_OKTA_DOMAIN,
+
+        okta_client_id: process.env.REACT_APP_OKTA_CLIENTID,
+    },
+})
