4f3782581d5e90a2579a89d228e1cc492c7c4480
Maximilian Mill <maximilianmuhlenberg@gmail.com>
2023-12-12 12:44:31 +0100

    Created e2e tests for story 1

---
+++ frontend/cypress/e2e/insertproposaltest/insertproposaltest.cy.js
@@ -0,0 +1,57 @@
+describe('Insert Proposal', () => {
+
+    beforeEach(() => {
+        cy.visit('http://localhost:5173');
+
+        cy.get('button').contains('Login').click();
+        cy.intercept('POST', 'https://dev-59214398.okta.com/**').as('oktaLogin');
+        cy.wait(6000);
+        cy.get('#email',{ timeout: 10000 }).type('p101@example.com');
+        cy.get('#password').type('Test.101');
+        cy.get('button').contains('sign in').click();
+
+        // Wait for the Okta login request to complete
+        cy.wait('@oktaLogin');
+
+        // visit the proposal creation page
+        cy.visit('http://localhost:5173/professorDashboard/addNewThesis/')
+    })
+
+    it('create a new proposal', () => {
+
+        cy.get('#title').type('Test Proposal')
+        cy.get('#coSupervisors').type('Giovanna Ruben')
+        cy.get('#keywords').type('AI ') // test that the keywords are added when typing space behind them
+        cy.get('#type').type('Research')
+        cy.get('#groups').type('G32')
+        cy.get('#description').type('Test description')
+        cy.get('#requiredKnowledge').type('Test knowledge')
+        cy.get('#notes').type('Test notes')
+        cy.get('#expiration').type('2024-04-30')
+        cy.get('#level').type('MSc')
+        cy.get('#cds').type('Computer Engineer')
+
+        cy.get('button').contains('Submit').click()
+
+        cy.get('.MuiAlert-message').should('not.be.visible')
+    })
+
+    it('submit proposal form without title', () => {
+
+        cy.get('#title').type('Test Proposal')
+        cy.get('#coSupervisors').type('Giovanna Ruben')
+        cy.get('#keywords').type('AI,') // test that the keywords are added when typing space behind them
+        cy.get('#type').type('Research')
+        cy.get('#groups').type('G32')
+        cy.get('#description').type('Test description')
+        cy.get('#requiredKnowledge').type('Test knowledge')
+        cy.get('#notes').type('Test notes')
+        cy.get('#expiration').type('2024-04-30')
+        cy.get('#level').type('MSc')
+        cy.get('#cds').type('Computer Engineer')
+
+        cy.get('button').contains('Submit').click()
+
+        cy.get('.MuiAlert-message').should('be.visible')
+    })
+})
